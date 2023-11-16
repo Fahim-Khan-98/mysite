fff188558269d709aac8f75d8dc91ec818ab3e16
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2023-11-16 17:06:51 +0600

    [Add]: PublishedManager

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context={
+        'posts' : posts
+    }
+    return render(request, 'blog/post_list.html', context)
+
+def publish_post_list(request):
+    posts = Post.published.all()
+    context={
+        'posts' : posts
+    }
+    return render(request, 'blog/post_list.html', context)
