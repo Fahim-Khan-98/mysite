a1a7f42b4d67854b8e0f818f2dfc32efa3d2bd45
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2023-11-18 11:48:55 +0600

    [Done]:Building a Blog Application

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name= 'blog'
 urlpatterns = [
     path('', views.post_list),
-    path('publish/post/list', views.publish_post_list),
+    # path('publish/post/list', views.publish_post_list),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Post
 # Create your views here.
 
@@ -12,9 +13,19 @@ def post_list(request):
     }
     return render(request, 'blog/post_list.html', context)
 
-def publish_post_list(request):
-    posts = Post.published.all()
+# def publish_post_list(request):
+#     posts = Post.published.all()
+#     context={
+#         'posts' : posts
+#     }
+#     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     context={
-        'posts' : posts
+        'post' : post,
+
     }
-    return render(request, 'blog/post_list.html', context)
+    return render(request,'blog/post_detail.html',context)
+
