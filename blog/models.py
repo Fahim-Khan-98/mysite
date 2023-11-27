90a552944a090327ba07943f4507e884d1e75f65
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2023-11-27 09:53:58 +0600

    [Add]: pagination

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 # Create your models here.
 
 class PublishedManager(models.Manager):
@@ -22,7 +23,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     publish = models.DateTimeField(default=timezone.now)
@@ -40,6 +41,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
     
     def get_absolute_url(self):
-       return reverse('blog:post_detail', args=[str(self.id)])
+       return reverse('blog:post_detail', args=[self.id])
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Post
 # Create your views here.
 
@@ -7,7 +8,19 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+
+    except EmptyPage:
+         # If page_number is out of range deliver last page of results
+         posts = paginator.page(paginator.num_pages)
     context={
         'posts' : posts
     }
